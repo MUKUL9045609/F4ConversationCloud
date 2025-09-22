51c90f7cf705beac9e88cdbcb717844abad6d356
Ashwin <ashwin.c@fortune4.in>
2025-09-22 10:42:18 +0530

    added services,repo,controller

---
+++ src/F4ConversationCloud/F4ConversationCloud.Infrastructure/Service/Client/ClientManagementService.cs
@@ -0,0 +1,13 @@
+﻿using F4ConversationCloud.Application.Common.Interfaces.Services.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F4ConversationCloud.Infrastructure.Service.Client
+{
+    public class ClientManagementService: IClientManagementService
+    {
+    }
+}
