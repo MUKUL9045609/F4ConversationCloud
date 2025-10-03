4df82cef432cbed823e70ee97e31d418a68c2118
Raj Maru <raj.m@fortune4.in>
2025-10-03 11:23:58 +0530

    Master Tax service and Repository created Master Tax Save and Edit functionality created

---
+++ src/F4ConversationCloud/F4ConversationCloud.Application/Common/Interfaces/Services/SuperAdmin/IMasterTaxService.cs
@@ -0,0 +1,15 @@
+﻿using F4ConversationCloud.Domain.Entities.SuperAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F4ConversationCloud.Application.Common.Interfaces.Services.SuperAdmin
+{
+    public interface IMasterTaxService
+    {
+        Task<int> CreateUpdateAsync(MasterTax masterTax);
+        Task<MasterTax> GetMasterTaxAsync();
+    }
+}
