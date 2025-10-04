5e5d6d1ac71e49e4c218e1eebbee8ebf0052c29e
Ashwin <ashwin.c@fortune4.in>
2025-10-04 17:37:20 +0530

    fiexed

--- src/F4ConversationCloud/F4ConversationCloud.SuperAdmin/Controllers/TemplateManagementController.cs
+++ src/F4ConversationCloud/F4ConversationCloud.SuperAdmin/Controllers/TemplateManagementController.cs
@@ -29,52 +29,60 @@ namespace F4ConversationCloud.SuperAdmin.Controllers
         [HttpPost("create-template")]
         public IActionResult CreateTemplate(CreateTemplateViewModel Request)        
         {
-           
-          
-            var components = (Request.Components ?? new List<ComponentViewModel>())
-                       .Where(c => !string.IsNullOrWhiteSpace(c.Text)
-                           || (c.Buttons?.Any(b => !string.IsNullOrWhiteSpace(b.Text)) == true)
-                           
-                           )
-                       .Select(c => new CreateTemplateComponent
-                       {
-                           Type = c.Type,
-                           Text = c.Text,
-                           Format = c.Format,
-                           Buttons = c.Buttons?
-                                   .Where(b => !string.IsNullOrWhiteSpace(b.Text))
-                                   .Select(b => new TemplateButton
-                                   {
-                                       Text = b.Text,
-                                       Type = b.Type,
-                                       Url = b.Url
-                                   }).ToList(),
-                           Example = c.Example != null
-                                            ? new Example
-                                            {
-                                                HeaderText = (c.Example.HeaderText?.Any() == true)
-                                                    ? c.Example.HeaderText
-                                                    : new List<string>(),
-
-                                                BodyText = c.Example.BodyText?.ToList()
-                                                    ?? new List<List<string>>()
-                                            
-                                            }
-                                 : null
-                       }).ToList();
-
-
-            var templateRequest = new WhatsAppTemplateRequest
+            try
             {
-                Name = Request.TemplateName,
-                Language = Request.Language,
-                Category = Request.Category,
-                Components = components
-            };
-            var jsoneserialiazer = JsonSerializer.Serialize(templateRequest);
-            var createTemplate = _templateManagementService.CreateTemplate(templateRequest);
+                var components = (Request.Components ?? new List<ComponentViewModel>())
+                                       .Where(c => !string.IsNullOrWhiteSpace(c.Text)
+                                           || (c.Buttons?.Any(b => !string.IsNullOrWhiteSpace(b.Text)) == true)
 
-            return View(Request);
+                                           )
+                                       .Select(c => new CreateTemplateComponent
+                                       {
+                                           Type = c.Type,
+                                           Text = c.Text,
+                                           Format = c.Format,
+                                           Buttons = c.Buttons?
+                                                   .Where(b => !string.IsNullOrWhiteSpace(b.Text))
+                                                   .Select(b => new TemplateButton
+                                                   {
+                                                       Text = b.Text,
+                                                       Type = b.Type,
+                                                       Url = b.Url
+                                                   }).ToList(),
+                                           Example = c.Example != null
+                                                            ? new Example
+                                                            {
+                                                                HeaderText = (c.Example.HeaderText?.Any() == true)
+                                                                    ? c.Example.HeaderText
+                                                                    : new List<string>(),
+
+                                                                BodyText = c.Example.BodyText?.ToList()
+                                                                    ?? new List<List<string>>()
+
+                                                            }
+                                                 : null
+                                       }).ToList();
+
+
+                var templateRequest = new WhatsAppTemplateRequest
+                {
+                    Name = Request.TemplateName,
+                    Language = Request.Language,
+                    Category = Request.Category,
+                    Components = components
+                };
+                var jsoneserialiazer = JsonSerializer.Serialize(templateRequest);
+                var createTemplate = _templateManagementService.CreateTemplate(templateRequest);
+
+                return View(Request);
+            }
+            catch (Exception)
+            {
+
+               return View(Request);
+            }
+          
+            
         }
     }
 }
