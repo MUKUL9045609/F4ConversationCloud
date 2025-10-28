0f47ff4ac93788640b5c4186f8a5ecb679aa5921
Raj Maru <raj.m@fortune4.in>
2025-10-28 17:55:55 +0530

    Super Admin log service and repository created

--- src/F4ConversationCloud/F4ConversationCloud.Infrastructure/Service/SuperAdmin/ClientRegistrationService.cs
+++ src/F4ConversationCloud/F4ConversationCloud.Infrastructure/Service/SuperAdmin/ClientRegistrationService.cs
@@ -18,14 +18,16 @@ namespace F4ConversationCloud.Infrastructure.Service.SuperAdmin
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IConfiguration _configuration;
         private readonly IEmailSenderService _emailSenderService;
+        private readonly ILogService _logService;
         public ClientRegistrationService(IClientRegistrationRepository clientRegistrationRepository, IWebHostEnvironment env,
-            IHttpContextAccessor httpContextAccessor, IConfiguration configuration, IEmailSenderService emailSenderService)
+            IHttpContextAccessor httpContextAccessor, IConfiguration configuration, IEmailSenderService emailSenderService, ILogService logService)
         {
             _clientRegistrationRepository = clientRegistrationRepository;
             _env = env;
             _httpContextAccessor = httpContextAccessor;
             _configuration = configuration;
             _emailSenderService = emailSenderService;
+            _logService = logService;
         }
 
         public async Task<int> CreateUpdateAsync(ClientRegistration clientRegistration)
@@ -52,34 +54,54 @@ namespace F4ConversationCloud.Infrastructure.Service.SuperAdmin
                 Email = cr.Email,
                 ContactNumber = cr.ContactNumber,
                 Role = cr.Role,
-                RegistrationStatus = cr.RegistrationStatus,
+                RegistrationStatus = cr.RegistrationStatus
             };
         }
 
         public async Task SendRegistrationEmailAsync(string email, string name, int id)
         {
-            string templatePath = Path.Combine(_env.WebRootPath, "Html", "RegistrationEmailTemplate.html");
-            string htmlBody = await File.ReadAllTextAsync(templatePath);
-
-            var request = _httpContextAccessor.HttpContext.Request;
-            string baseUrl = $"{request.Scheme}://{request.Host}";
-            string encryptedId = id.ToString().Encrypt();
-
-            string registrationLink = $"{_configuration["OnboardingUrl"]}?id={encryptedId}";
-            string currentYear = DateTime.Now.Year.ToString();
-            htmlBody = htmlBody.Replace("{user_name}", name)
-                               .Replace("{BaseUrl}", baseUrl)
-                               .Replace("{Link}", registrationLink)
-                               .Replace("{CurrentYear}", currentYear);
-
-            var emailRequest = new EmailRequest
+            var model = new LogModel();
+            try
             {
-                ToEmail = email,
-                Subject = "Complete Your Registration",
-                Body = htmlBody
-            };
+                string templatePath = Path.Combine(_env.WebRootPath, "Html", "RegistrationEmailTemplate.html");
+                string htmlBody = await File.ReadAllTextAsync(templatePath);
 
-            await _emailSenderService.Send(emailRequest);
+                var request = _httpContextAccessor.HttpContext.Request;
+                string baseUrl = $"{request.Scheme}://{request.Host}";
+                string encryptedId = id.ToString().Encrypt();
+
+                string registrationLink = $"{_configuration["OnboardingUrl"]}?id={encryptedId}";
+                string currentYear = DateTime.Now.Year.ToString();
+                htmlBody = htmlBody.Replace("{user_name}", name)
+                                   .Replace("{BaseUrl}", baseUrl)
+                                   .Replace("{Link}", registrationLink)
+                                   .Replace("{CurrentYear}", currentYear);
+
+                var emailRequest = new EmailRequest
+                {
+                    ToEmail = email,
+                    Subject = "Complete Your Registration",
+                    Body = htmlBody
+                };
+
+                await _emailSenderService.Send(emailRequest);
+
+                model.LogType = "Success";
+                model.Message = "Registration email sent successfully.";
+                model.AdditionalInfo = $"Email: {email}, Name: {name}, ID: {id}";
+            }
+            catch (Exception ex)
+            {
+                model.Source = "SendRegistrationEmailAsync";
+                model.LogType = "Error";
+                model.Message = ex.Message;
+                model.StackTrace = ex.StackTrace;
+                model.AdditionalInfo = $"Email: {email}, Name: {name}, ID: {id}";
+            }
+            finally
+            {
+                await _logService.InsertLogAsync(model);
+            }
         }
     }
 }
