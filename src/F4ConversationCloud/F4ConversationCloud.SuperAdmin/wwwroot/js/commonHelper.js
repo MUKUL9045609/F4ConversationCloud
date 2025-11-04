3c21fdddaa791d4f947a679c2c7ba8ec329924ef
Raj Maru <raj.m@fortune4.in>
2025-11-04 16:49:24 +0530

    toast msg commonHelper fixes

--- src/F4ConversationCloud/F4ConversationCloud.SuperAdmin/wwwroot/js/commonHelper.js
+++ src/F4ConversationCloud/F4ConversationCloud.SuperAdmin/wwwroot/js/commonHelper.js
@@ -27,7 +27,7 @@
     that.showSuccess = function (Message) {
         var id = that.generateGUID();
         var html = that.getToasterHTML();
-        html = html.replace('{class}', 'toast--green'); // Keep Toastify green class
+        html = html.replace('{class}', 'toast--green');
         html = html.replace('{Id}', id);
         html = html.replace('{message}', Message);
 
@@ -38,7 +38,7 @@
             $('#' + id).closest('.toastify').fadeOut(400, function () {
                 $(this).remove();
             });
-        }, that.ToasterSeconds * 1000000);
+        }, that.ToasterSeconds * 1000);
     };
 
     that.showError = function (Message) {
@@ -54,7 +54,7 @@
             $('#' + id).closest('.toastify').fadeOut(400, function () {
                 $(this).remove();
             });
-        }, that.ToasterSeconds * 100000);
+        }, that.ToasterSeconds * 1000);
     };
 
     that.showInfo = function (Message) {
@@ -70,7 +70,7 @@
             $('#' + id).closest('.toastify').fadeOut(400, function () {
                 $(this).remove();
             });
-        }, that.ToasterSeconds * 100000);
+        }, that.ToasterSeconds * 1000);
     };
 
     that.showWarning = function (Message) {
@@ -86,7 +86,7 @@
             $('#' + id).closest('.toastify').fadeOut(400, function () {
                 $(this).remove();
             });
-        }, that.ToasterSeconds * 100000);
+        }, that.ToasterSeconds * 1000);
     };
 
     that.getToasterHTML = function () {
