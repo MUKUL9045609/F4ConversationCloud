984c4bc3dc9e6aad76ff5498e7fd9958f0a60dd6
Mukul9045609 <mukul.s@fortune4.in>
2025-11-05 10:15:26 +0530

    made changes for meta repositories

---
+++ src/F4ConversationCloud/F4ConversationCloud.Application/Common/Interfaces/Repositories/IMetaRepositories.cs
@@ -0,0 +1,14 @@
+﻿using F4ConversationCloud.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F4ConversationCloud.Application.Common.Interfaces.Repositories
+{
+    public interface IMetaRepositories
+    {
+        Task<int> UpdateMetaUsersConfigurationDetails(WhatsAppAccountTableModel request);
+    }
+}
