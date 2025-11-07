17f724f4a6103ac297924bd7b4586f990d8a5a09
Ashwin <ashwin.c@fortune4.in>
2025-11-07 11:26:23 +0530

    added template service repo

---
+++ src/F4ConversationCloud/F4ConversationCloud.ClientAdmin/Controllers/TemplateManagemetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace F4ConversationCloud.ClientAdmin.Controllers
+{
+    public class TemplateManagemetController : BaseController
+    {
+        public TemplateManagemetController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
