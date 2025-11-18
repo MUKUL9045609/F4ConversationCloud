2e3cca83055aa2666af75ccee31e6df9a393f426
Raj Maru <raj.m@fortune4.in>
2025-11-18 17:27:42 +0530

    button logo added

--- src/F4ConversationCloud/F4ConversationCloud.Domain/Enum/ButtonCategory.cs
+++ src/F4ConversationCloud/F4ConversationCloud.Domain/Enum/ButtonCategory.cs
@@ -16,6 +16,22 @@ namespace F4ConversationCloud.Domain.Enum
 
         [Description("Visit website")]
         [Display(Name = "Visit website")]
-        VisitWebsite = 2
+        VisitWebsite = 2,
+
+        [Description("Call on WhatsApp")]
+        [Display(Name = "Call on WhatsApp")]
+        CallOnWhatsApp = 3,
+
+        [Description("Call phone number")]
+        [Display(Name = "Call phone number")]
+        CallPhoneNumber = 4,
+
+        [Description("Complete Flow")]
+        [Display(Name = "Complete Flow")]
+        CompleteFlow = 5,
+
+        [Description("Copy offer code")]
+        [Display(Name = "Copy offer code")]
+        CopyOfferCode = 6
     }
 }
