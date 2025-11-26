a4d70af4bbd6d861ea4684edff8db1f8e3d37eaf
Mukul9045609 <mukul.s@fortune4.in>
2025-11-26 10:39:34 +0530

    Made changes for Insert send Template Insight changes

--- src/F4ConversationCloud/F4ConversationCloud.Infrastructure/Repositories/SendTemplateMessageRepository.cs
+++ src/F4ConversationCloud/F4ConversationCloud.Infrastructure/Repositories/SendTemplateMessageRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using F4ConversationCloud.Application.Common.Models;
+using F4ConversationCloud.Infrastructure.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,25 +11,32 @@ namespace F4ConversationCloud.Infrastructure.Repositories
 {
     public class SendTemplateMessageRepository
     {
-        public SendTemplateMessageRepository()
+        private readonly IGenericRepository<string> _repository;
+        public SendTemplateMessageRepository(IGenericRepository<string> repository)
         {
-
+            _repository = repository;
         }
 
 
-        //public async Task<int> Insert(WhatsAppAccountTableModel request)
-        //{
-        //    DynamicParameters parameters = new DynamicParameters();
+        public async Task<int> Insert(WhatsAppAccountTableModel request)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@ClientInfoId", request.BusinessId);
+                parameters.Add("WABAId", request.WABAId);
+                parameters.Add("BusinessCategory", request.BusinessCategory);
+                parameters.Add("WhatsAppDisplayName", request.WhatsAppDisplayName);
+                parameters.Add("PhoneNumberId", request.PhoneNumberId);
+                parameters.Add("PhoneNumber", request.PhoneNumber);
+                parameters.Add("Status", request.Status);
 
-        //    parameters.Add("BusinessId", request.BusinessId);
-        //    parameters.Add("WABAId", request.WABAId);
-        //    parameters.Add("BusinessCategory", request.BusinessCategory);
-        //    parameters.Add("WhatsAppDisplayName", request.WhatsAppDisplayName);
-        //    parameters.Add("PhoneNumberId", request.PhoneNumberId);
-        //    parameters.Add("PhoneNumber", request.PhoneNumber);
-        //    parameters.Add("Status", request.Status);
-
-        //    return await _repository.GetCountAsync("sp_InsertOrUpdate_WhatsAppAccount", parameters);
-        //}
+                return await _repository.GetCountAsync("Sp_InsertTemplateInsights", parameters);
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
     }
 }
